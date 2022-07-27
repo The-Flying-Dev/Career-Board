89b78c109f64b1f065f0ab921ce7b56ae3d32d53
The-Flying-Dev <leandranm@outlook.com>
2022-07-26 17:58:25 -0700

    Rails Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
+
+  namespace :api do
+    namespace :v1 do
+      get '/jobs', to: 'jobs#index'
+      post '/jobs', to: 'jobs#create'
+      get '/jobs/:id', to: 'jobs#show'
+      patch '/jobs/:id', to: 'jobs#update'
+      delete '/jobs/:id', to: 'jobs#destroy'
+    end
+  end
+
   root 'homes#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
