c442dc2b4e37ee40f26c408a245004c4917b8c42
The-Flying-Dev <leandranm@outlook.com>
2022-07-26 18:26:48 -0700

    Seed data and install axios

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Job.create(
+  company: 'Amazon',
+  position: 'Staff Software Engineer',
+  description: 'Be the lead technical resource'
+)
+
+Job.create(
+  company: 'Youtube',
+  position: 'Data Scientist',
+  description: 'make sense of data'
+)
+
+Job.create(
+  company: 'KPMG',
+  position: 'CEO',
+  description: 'Lead the company growth'
+)
